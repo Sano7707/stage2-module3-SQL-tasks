28428a764c40fc6396779b89d8cf6ae9ce3dead3
Sano <sanasar.hambardzumyan@gmail.com>
2023-01-18 22:35:43 +0400

    Changes

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
 delete from student where groupnumber >= 4;
 delete from student where id in (select student_id from mark where mark < 4);
 delete from paymenttype where id = 1;
-delete from mark where mark < 7;
+delete from mark where mark <= 7;
--- lesson9/src/main/resources/SOLUTION.sql
+++ lesson9/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
-select * from student join mark on student.id = mark.student_id having AVG(mark) > 8;
+select * from student where id in (select student_id from mark where AVG(mark) > 8);
 select id,name from student join mark on student.id = mark.student_id having min(mark) > 7;
 select id,name from student join payment on student.id = payment.student_id where COUNT(*) > 2 and payment_date > '2019-01-01' and payment_date < '2020-01-01';
 
