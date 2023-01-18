d8a4617365b98289825467bb0988f6de33143069
Sano <sanasar.hambardzumyan@gmail.com>
2023-01-18 23:20:47 +0400

    Changes

--- lesson12/src/main/resources/TABLES_CHANGES.sql
+++ lesson12/src/main/resources/TABLES_CHANGES.sql
@@ -1,5 +1,4 @@
-alter table mark drop constraint mark_student_id_fkey;
-ALTER TABLE mark ADD CONSTRAINT  mark_student_id_fkey FOREIGN KEY (student_id) REFERENCES student(id)  ON DELETE CASCADE;
+
 alter table payment drop constraint payment_student_id_fkey;
 ALTER TABLE payment ADD CONSTRAINT  payment_student_id_fkey FOREIGN KEY (student_id) REFERENCES student(id)  ON DELETE CASCADE;
 alter table payment drop constraint payment_type_id_fkey;
