d5383ef442b55dfa4498c855856c905c6ad081c1
Sano <sanasar.hambardzumyan@gmail.com>
2023-01-18 22:53:02 +0400

    Changes

--- lesson3/src/main/resources/SOLUTION.sql
+++ lesson3/src/main/resources/SOLUTION.sql
@@ -2,7 +2,7 @@ ALTER TABLE student ALTER COLUMN birthday SET NOT NULL;
 ALTER TABLE mark ALTER COLUMN student_id SET NOT NULL;
 ALTER TABLE mark ALTER COLUMN subject_id SET NOT NULL;
 ALTER TABLE mark ADD CONSTRAINT TEST_CONST CHECK mark >= 1 AND mark <= 10;
-ALTER TABLE sybject ADD CONSTRAINT grade_check CHECK grade > 1 AND grade < 5;
+ALTER TABLE subject ADD CONSTRAINT grade_check CHECK grade >= 1 AND grade <= 5;
 ALTER TABLE paymenttype ADD CONSTRAINT UQ_product_name UNIQUE(name);
 ALTER TABLE payment ALTER COLUMN type_id SET NOT NULL;
 ALTER TABLE payment ALTER COLUMN amount SET NOT NULL;
--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -1,8 +1,8 @@
 select * from payment where amount >= 500;
 select * from student where dateadd(year,20,birthday) < '20230117';
-select * from student where groupnumber = 10 AND dateadd(year,20,birthday) < '20230117';
-select * from student where name = 'Mike' or groupnumber = 4 or groupnumber = 5 or groupnumber = 6;
+select * from student where groupnumber = 10 AND dateadd(year,20,birthday) > '20230117';
+select * from student where name = 'Mike' and (groupnumber = 4 or groupnumber = 5 or groupnumber = 6);
 select * from payment where dateadd(month, 8, payment_date) > dateadd(month, -8, '20230117');
 select * from student where name Like 'A%';
-select * from student where name = 'Roxi' and groupnumber = 4 OR name = 'Tallie' and groupnumber = 9;
+select * from student where (name = 'Roxi' and groupnumber = 4) or (name = 'Tallie' and groupnumber = 9);
 
